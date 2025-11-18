cb930b4c321fd18f6e14ac4afeb651393460f816
Harsh Singh <Harshroop100@gmail.com>
2025-11-18 09:07:31 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,9 +3,6 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
