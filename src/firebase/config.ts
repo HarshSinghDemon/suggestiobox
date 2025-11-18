661c103ed5052e96d2b362afdef01db5d40bcc90
Harsh Singh <Harshroop100@gmail.com>
2025-11-18 13:09:24 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,6 @@ export const firebaseConfig = {
   "appId": "1:362152778766:web:7223df0af29547f163b05f",
   "apiKey": "AIzaSyAAUUlHnvD8F9XoIYvIJhRTmzWlrUtJjxY",
   "authDomain": "studio-1865523494-6b2c8.firebaseapp.com",
-  "storageBucket": "studio-1865523494-6b2c8.appspot.com",
   "measurementId": "",
   "messagingSenderId": "362152778766"
 };
