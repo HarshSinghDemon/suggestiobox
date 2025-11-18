667656e33cb375a66fb89c4e438bdff189938337
Harsh Singh <Harshroop100@gmail.com>
2025-11-18 09:00:22 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,7 +1,11 @@
+
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
