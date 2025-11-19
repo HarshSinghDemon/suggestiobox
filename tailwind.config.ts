0c4abaa7d5b6315dcb21055db04d595935a85c10
Harsh Singh <Harshroop100@gmail.com>
2025-11-19 09:31:51 +0000

    its too fast make it random smooth not this fast (_for element <LinkComp

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -119,7 +119,7 @@ export default {
         'pulse-scale': 'pulse-scale 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
         'shake': 'shake 0.5s cubic-bezier(.36,.07,.19,.97) both',
         'shine': 'shine 2s linear infinite',
-        'wave': 'wave 3s ease-in-out infinite',
+        'wave': 'wave 5s ease-in-out infinite',
       },
     },
   },
