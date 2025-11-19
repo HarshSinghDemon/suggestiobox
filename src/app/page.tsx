321759ed25c52ab2cb4536ba103002b57d9c94a1
Harsh Singh <Harshroop100@gmail.com>
2025-11-19 16:17:27 +0000

    make it look good and pop out attractive animations (_for element p_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
 
             <div className="pt-8">
               <p className="text-sm text-foreground/60 font-arcade">
-                  Made with ❤️ by 5B
+                  Made with <span className='inline-block text-red-500 animate-heartbeat'>❤️</span> by 5B
               </p>
             </div>
           </div>
