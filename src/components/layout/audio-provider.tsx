f3050cfc8666b3912d735f2183e6a006801b8fce
Harsh Singh <Harshroop100@gmail.com>
2025-11-20 20:24:45 +0000

    https://ik.imagekit.io/bt0k47tzc/green_hill_zone.mp3?updatedAt=176367009

--- src/components/layout/audio-provider.tsx
+++ src/components/layout/audio-provider.tsx
@@ -17,6 +17,13 @@ const musicTracks = [
     { title: "Cadillacs & Dinosaurs - Select", url: "https://ryvsxwjnldugnwxjhgem.supabase.co/storage/v1/object/public/uploads/music/Cadillacs%20And%20Dinosaurs%20-%20Player%20Select.mp3" },
     { title: "Pokemon Original Composition", url: "https://ryvsxwjnldugnwxjhgem.supabase.co/storage/v1/object/public/uploads/music/Pokimon%20Original%20Composition.mp3" },
     { title: "Street Fighter II - Opening", url: "https://ryvsxwjnldugnwxjhgem.supabase.co/storage/v1/object/public/uploads/music/Street%20Fighter%20II%20Arcade%20Music%20-%20Opening%20Theme%20-%20CPS1.mp3" },
+    { title: "Green Hill Zone", url: "https://ik.imagekit.io/bt0k47tzc/green_hill_zone.mp3?updatedAt=1763670098871" },
+    { title: "Super Mario Bros. Theme", url: "https://ik.imagekit.io/bt0k47tzc/supermario.mp3?updatedAt=1763670098578" },
+    { title: "Mega Man 2 - Dr. Wily", url: "https://ik.imagekit.io/bt0k47tzc/megaman.mp3?updatedAt=1763670097812" },
+    { title: "Dragon Ball Z - Cha-La", url: "https://ik.imagekit.io/bt0k47tzc/dragon_ball.mp3?updatedAt=1763670096714" },
+    { title: "Ryu's Theme", url: "https://ik.imagekit.io/bt0k47tzc/ryu.mp3?updatedAt=1763670096135" },
+    { title: "Ben 10 Theme", url: "https://ik.imagekit.io/bt0k47tzc/ben10.mp3?updatedAt=1763670095075" },
+    { title: "Pokémon Theme", url: "https://ik.imagekit.io/bt0k47tzc/pokemon.mp3?updatedAt=1763670094935" },
 ];
 
 type Track = {
