5efc934238c1f2d8a10825c99e4e2019693d8fb6
Harsh Singh <Harshroop100@gmail.com>
2025-11-20 11:20:45 +0000

    increases the game play container size increase the display area of the

--- src/app/community-games/brick-breaker/page.tsx
+++ src/app/community-games/brick-breaker/page.tsx
@@ -11,22 +11,20 @@ export default function BrickBreakerPage() {
     const { user } = useUser();
   return (
     <AuthWrapper>
-      <div className="container flex-grow py-8 mx-auto">
-        <div className="grid h-full grid-cols-1 lg:grid-cols-3 gap-8">
-            <div className="lg:col-span-2 h-full">
-                <Card className="flex flex-col h-full">
-                    <CardHeader className="text-center">
-                        <CardTitle>Brick Breaker</CardTitle>
-                        <CardDescription>Use your mouse or finger to move the paddle. Clear all the bricks!</CardDescription>
-                    </CardHeader>
-                    <CardContent className="flex-1 p-0">
-                        <BrickBreakerGame />
-                    </CardContent>
-                </Card>
-            </div>
-            <div className="lg:col-span-1">
-                {user && <Leaderboard gameId="brick-breaker" />}
-            </div>
+      <div className="flex flex-col h-[calc(100vh-4rem)]">
+        <div className="flex-grow">
+            <Card className="flex flex-col h-full border-0 rounded-none">
+                <CardHeader className="text-center">
+                    <CardTitle>Brick Breaker</CardTitle>
+                    <CardDescription>Use your mouse or finger to move the paddle. Clear all the bricks!</CardDescription>
+                </CardHeader>
+                <CardContent className="flex-1 p-0">
+                    <BrickBreakerGame />
+                </CardContent>
+            </Card>
+        </div>
+        <div className="p-4 bg-background">
+            {user && <Leaderboard gameId="brick-breaker" />}
         </div>
       </div>
     </AuthWrapper>
