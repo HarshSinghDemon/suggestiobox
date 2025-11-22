9541fc77b75aeff3035c984f2b37285a976db57d
Harsh Singh <Harshroop100@gmail.com>
2025-11-22 13:56:57 +0000

    pick the image from elsewhere dont us imagekit (_for element img_)

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -28,7 +28,7 @@ function WelcomePanel() {
         <div className="flex flex-col items-center justify-center h-full gap-6 text-center animate-fade-in-scale">
             <div className="relative w-56 h-56 animate-pulse-slow">
                 <Image 
-                    src="https://ik.imagekit.io/bt0k47tzc/undraw_chatting_re_j55r_1.svg?updatedAt=1764121287955"
+                    src="https://ryvsxwjnldugnwxjhgem.supabase.co/storage/v1/object/public/uploads/website-images/undraw_chatting_re_j55r.svg"
                     alt="Select a chat"
                     fill
                     className="object-contain"
